234cf67933b7de4ba74bbc1edab28b4ebf410481
Dzianis Shybeka <DzianisShybeka@eharmony.com>
2013-10-07 18:37:04 +0300

    Updated maven-ant-task dependency. Moved to third-party repository.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,11 +14,12 @@ grails.project.dependency.resolution = {
 
         mavenLocal()
         mavenCentral()
+        mavenRepo "https://repository.corp.eharmony.com/nexus/content/repositories/thirdparty"
     }
 
     dependencies {
         compile "org.apache.ivy:ivy:2.2.0"
-        compile("org.apache.maven:maven-ant-tasks:2.1.4-SNAPSHOT") {
+        compile("org.apache.maven:maven-ant-tasks:2.1.4") {
             excludes "commons-logging", "xml-apis", "groovy"
         }
         test("org.gmock:gmock:0.8.0") {
@@ -27,11 +28,12 @@ grails.project.dependency.resolution = {
     }
 }
 
-//grails.project.repos.'manageReleases'.url = "https://repository.corp.eharmony.com/nexus/content/repositories/internal-snapshots/"
-//grails.project.repos.'manageReleases'.username = "dshybeka"
-//grails.project.repos.'manageReleases'.password = "IZC7hlzh00"
+grails.project.repos.'eh-thirdparty'.url = "https://repository.corp.eharmony.com/nexus/content/repositories/internal-snapshots/"
+grails.project.repos.'eh-thirdparty'.username = "dshybeka"
+grails.project.repos.'eh-thirdparty'.password = "IZC7hlzh00"
+grails.project.repos.default = "eh-thirdparty"
 
-grails.project.repos.'plugin-repo'.url = "http://localhost:8081/nexus/content/repositories/snapshots/"
-grails.project.repos.'plugin-repo'.username = "admin"
-grails.project.repos.'plugin-repo'.password = "admin123"
-grails.project.repos.default = "plugin-repo"
+//grails.project.repos.'plugin-repo'.url = "http://localhost:8081/nexus/content/repositories/snapshots/"
+//grails.project.repos.'plugin-repo'.username = "admin"
+//grails.project.repos.'plugin-repo'.password = "admin123"
+//grails.project.repos.default = "plugin-repo"
--- readme.txt
+++ readme.txt
@@ -1,3 +1,3 @@
 This plugin use repositories from BuildConfig.groovy by default instead of settings.xml.
 
-Plese use 'grails maven-deploy --repository=manageReleases' to upload this plugin to remote nexus and then use it to deploy your own Grails plojects to nexus.
+Plese use 'grails maven-deploy --repository=eh-thirdparty' to upload this plugin to remote nexus and then use it to deploy your own Grails plojects to nexus.
